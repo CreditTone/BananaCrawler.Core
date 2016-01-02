1c48240cd2f4befc38d4fa98dba9ede034cf618a
zhongguo <zhongguo@creditease.cn>
2016-01-02 17:43:30 +0800

    fix

--- src/main/java/com/banana/common/master/ICrawlerMasterServer.java
+++ src/main/java/com/banana/common/master/ICrawlerMasterServer.java
@@ -1,19 +1,21 @@
 package com.banana.common.master;
 
+import java.io.Serializable;
 import java.rmi.Remote;
 import java.util.List;
 
-import com.banana.common.NodeStatus;
 import com.banana.request.BasicRequest;
 
 public interface ICrawlerMasterServer extends Remote{
 
-	void addDownloadNode(String host,int port) throws java.rmi.RemoteException;
+	void registerDownloadNode(String rmiAddress) throws java.rmi.RemoteException;
 	
 	void startTask() throws java.rmi.RemoteException;
 	
 	Object getTaskPropertie(String taskName,String name) throws java.rmi.RemoteException;
 	
 	void pushTaskRequests(String taskName,List<BasicRequest> requests) throws java.rmi.RemoteException;
+	
+	Object getStartContextAttribute(String taskName,String hashCode,String attribute) throws java.rmi.RemoteException;
 	
 }
