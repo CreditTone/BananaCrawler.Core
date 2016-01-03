8467d3a416a92d91ea2cfb79b471c88622734789
zhongguo <zhongguo@creditease.cn>
2016-01-03 21:34:50 +0800

    fix

--- src/main/java/com/banana/common/PropertiesNamespace.java
+++ src/main/java/com/banana/common/PropertiesNamespace.java
@@ -3,9 +3,11 @@ package com.banana.common;
 public final class PropertiesNamespace {
 	
 	public static final class Task{
-		
 		public static final String MAX_PAGE_RETRY_COUNT = "banana.task.page.maxretrycount";
-		
+	}
+	
+	public static final class Download{
+		public static final String MASTER_HOST = "banana.download.master.host";
 	}
 	
 }
