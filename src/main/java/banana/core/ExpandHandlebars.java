cc41a83e4b97927cf25fa6bea8d162e59cafa274
zhongguo <zhongguo@creditease.cn>
2016-09-14 17:38:02 +0800

    imple handlebars

---
+++ src/main/java/banana/core/ExpandHandlebars.java
@@ -0,0 +1,161 @@
+package banana.core;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.NameValuePair;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Template;
+
+import banana.core.util.URLEncodedUtils;
+
+public class ExpandHandlebars extends Handlebars {
+
+	public ExpandHandlebars() {
+		registerHelper("add", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				int sum = 0;
+				int p0 = 0;
+				for (int i = 0; i < options.params.length; i++) {
+					p0 = Integer.parseInt(options.param(i).toString());
+					sum += p0;
+				}
+				return sum;
+			}
+		});
+		registerHelper("sub", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				int p0 = options.param(0);
+				for (int i = 1; i < options.params.length; i++) {
+					int p = Integer.parseInt(options.param(i).toString());
+					p0 -= p;
+				}
+				return p0;
+			}
+		});
+		registerHelper("multiply", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				int product = Integer.parseInt(options.param(0).toString());
+				int p0 ;
+				for (int i = 1; i < options.params.length; i++) {
+					p0 = Integer.parseInt(options.param(i).toString());
+					product *= p0;
+				}
+				return product;
+			}
+		});
+		registerHelper("gt", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				float p0 = Float.parseFloat(((Object)options.param(0)).toString());
+				float p1 = Float.parseFloat(((Object)options.param(1)).toString());
+				return p0 > p1;
+			}
+		});
+		registerHelper("lt", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				float p0 = Float.parseFloat(((Object)options.param(0)).toString());
+				float p1 = Float.parseFloat(((Object)options.param(1)).toString());
+				return p0 < p1;
+			}
+		});
+		registerHelper("eq", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				Object s1 = options.param(0);
+				Object s2 = options.param(1);
+				return s1.equals(s2);
+			}
+		});
+		registerHelper("fixKey", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				String url = options.param(0);
+				if (!url.contains("?")){
+					return url;
+				}
+				String[] urlData  = url.split("\\?");
+				String baseUrl = urlData[0];
+				String querys  = urlData[1];
+				List<NameValuePair> pair = URLEncodedUtils.parse(querys);
+				for (int i = 1; i < options.params.length; i++) {
+					for (NameValuePair nvPair : pair) {
+						if (nvPair.getName().equals(options.param(i))){
+							pair.remove(nvPair);
+							break;
+						}
+					}
+				}
+				if (!pair.isEmpty()){
+					baseUrl += "?";
+					Iterator<NameValuePair> iter = pair.iterator();
+					NameValuePair nvPair = null;
+					while(iter.hasNext()){
+						nvPair = iter.next();
+						baseUrl += nvPair.getName() + "=" + nvPair.getValue();
+						if (iter.hasNext()){
+							baseUrl += "&";
+						}
+					}
+				}
+				return baseUrl;
+			}
+		});
+		registerHelper("contains", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				String content = options.param(0);
+				for (int i = 1; i < options.params.length; i++) {
+					if (!content.contains(options.param(i).toString())){
+						return false;
+					}
+				}
+				return true;
+			}
+		});
+		registerHelper("for", new Helper<Object>() {
+
+			public Object apply(Object context, Options options) throws IOException {
+				List<Map<String,Object>> result = new ArrayList<Map<String,Object>>();
+				String key = options.param(0);
+				int start = Integer.parseInt(options.param(1).toString());
+				int end = Integer.parseInt(options.param(2).toString());
+				Map<String,Object> dataDef = (Map<String, Object>) options.context.model();
+				Map<String,Object> iter_context = new HashMap<>();
+				for (int i = start; i < end; i++) {
+					iter_context.put(key, i);
+					Map<String,Object> item = new HashMap<String,Object>();
+					for (Entry<String,Object> def : dataDef.entrySet()) {
+						Template template = ExpandHandlebars.this.compileInline(def.getValue().toString());
+						item.put(def.getKey(), template.apply(iter_context));
+					}
+					result.add(item);
+				}
+				dataDef.put("_data", result);
+				return true;
+			}
+		});
+	}
+	
+	public List<Map<String,Object>> toFor(Map<String,Object> iterDef) throws IOException{
+		iterDef = new HashMap<String,Object>(iterDef);
+		String forDef = (String) iterDef.remove("for");
+		Template template = compileInline(forDef);
+		template.apply(iterDef);
+		return (List<Map<String, Object>>) iterDef.remove("_data");
+	} 
+	
+}
--- src/main/java/banana/core/protocol/Task.java
+++ src/main/java/banana/core/protocol/Task.java
@@ -20,65 +20,19 @@ public final class Task implements Writable{
 	
 	public static class Seed{
 		
-		protected String url;
+		public String url;
 		
-		protected String[] urls;
+		public String[] urls;
 		
-		protected String method;
+		public Map<String,Object> url_iterator;
 		
-		protected Map<String,String> headers;
+		public String method;
 		
-		protected Map<String,String> params;
+		public Map<String,String> headers;
 		
-		protected String processor;
-
-		public String[] getUrls() {
-			return urls;
-		}
-
-		public void setUrls(String[] urls) {
-			this.urls = urls;
-		}
-
-		public String getUrl() {
-			return url;
-		}
-
-		public void setUrl(String url) {
-			this.url = url;
-		}
-
-		public String getMethod() {
-			return method;
-		}
-
-		public void setMethod(String method) {
-			this.method = method;
-		}
-
-		public Map<String, String> getHeaders() {
-			return headers;
-		}
-
-		public void setHeaders(Map<String, String> headers) {
-			this.headers = headers;
-		}
-
-		public Map<String, String> getParams() {
-			return params;
-		}
-
-		public void setParams(Map<String, String> params) {
-			this.params = params;
-		}
-
-		public String getProcessor() {
-			return processor;
-		}
-
-		public void setProcessor(String processor) {
-			this.processor = processor;
-		}
+		public Map<String,String> params;
+		
+		public String processor;
 	}
 	
 	public static class ExpandableHashMap extends HashMap<String,Object>{
@@ -226,7 +180,7 @@ public final class Task implements Writable{
 			throw new Exception("There is no seed");
 		}
 		for (Seed seed : seeds) {
-			if (seed.url == null && seed.urls == null){
+			if (seed.url == null && seed.urls == null && seed.url_iterator == null){
 				throw new NullPointerException("seed url cannot be null");
 			}
 			if (seed.processor == null){
